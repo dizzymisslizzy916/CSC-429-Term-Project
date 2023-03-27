b960adbfae12009d161f1567b346cf4eaff4f6a0
dizzymisslizzy916 <113558563+dizzymisslizzy916@users.noreply.github.com>
2023-03-27 17:58:10 -0400

    Add files via upload

---
+++ Term_Project/model/TransactionFactory.java
@@ -0,0 +1,80 @@
+// specify the package
+package model;
+
+// system imports
+import java.util.Vector;
+import javax.swing.JFrame;
+
+// project imports
+
+/** The class containing the TransactionFactory for the ATM application */
+//==============================================================
+public class TransactionFactory
+{
+
+	/**
+	 *
+	 */
+	//----------------------------------------------------------
+	public static Transaction createTransaction(String transType)
+		throws Exception
+	{
+		Transaction retValue = null;
+
+		if (transType.equals("AddScout") == true)
+		{
+			retValue = new InsertScoutTransaction(cust);
+		}
+		else
+		if (transType.equals("AddTree") == true)
+		{
+			retValue = new InsertTreeTransaction(cust);
+		}
+		else
+		if (transType.equals("AddTreeType") == true)
+		{
+			retValue = new InsertTreeTypeTransaction(cust);
+		}
+		else
+		if (transType.equals("UpdateTree") == true)
+		{
+			retValue = new TreeTransaction(cust);
+		}
+		else
+		if (transType.equals("UpdateTreeType") == true)
+		{
+			retValue = new UpdateTreeTypeTransaction(cust);
+		}
+		else
+		if (transType.equals("UpdateScout") == true)
+		{
+			retValue = new UpdateScoutTransaction(cust);
+		}
+		else
+		if (transType.equals("SellTree") == true)
+		{
+			retValue = new TreeTransaction(cust);
+		}
+		else
+		if (transType.equals("RemoveTree") == true)
+		{
+			retValue = new TreeTransaction(cust);
+		}
+		else
+		if (transType.equals("RemoveScout") == true)
+		{
+			retValue = new UpdateScoutTransaction(cust);
+		}
+		else
+		if (transType.equals("StartShift") == true)
+		{
+			retValue = new StartShiftTransaction(cust);
+		}
+		else
+		if (transType.equals("EndShift") == true)
+		{
+			retValue = new EndShiftTransaction(cust);
+		}
+		return retValue;
+	}
+}
