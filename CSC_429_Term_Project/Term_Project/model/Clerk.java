fcc45dfbe63eef7b51736036e0bb20b2cabb512c
dizzymisslizzy916 <113558563+dizzymisslizzy916@users.noreply.github.com>
2023-03-28 18:11:30 -0400

    Add files via upload

---
+++ CSC_429_Term_Project/Term_Project/model/Clerk.java
@@ -0,0 +1,212 @@
+// specify the package
+package model;
+
+// system imports
+import java.util.Hashtable;
+import java.util.Properties;
+
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+
+// project imports
+import impresario.IModel;
+import impresario.ISlideShow;
+import impresario.IView;
+import impresario.ModelRegistry;
+
+import exception.InvalidPrimaryKeyException;
+import exception.PasswordMismatchException;
+import event.Event;
+import userinterface.MainStageContainer;
+import userinterface.View;
+import userinterface.ViewFactory;
+import userinterface.WindowPosition;
+
+public class Clerk implements IView, IModel{
+
+    // For Impresario
+    private Properties dependencies;
+    private ModelRegistry myRegistry;
+
+    // GUI Components
+    private Hashtable<String, Scene> myViews;
+    private Stage	  	myStage;
+
+    private String loginErrorMessage = "";
+    private String transactionErrorMessage = "";
+
+    public Clerk() //constructor
+    {
+        myStage = MainStageContainer.getInstance();
+        myViews = new Hashtable<String, Scene>();
+
+        // STEP 3.1: Create the Registry object - if you inherit from
+        // EntityBase, this is done for you. Otherwise, you do it yourself
+        myRegistry = new ModelRegistry("Clerk");
+        if(myRegistry == null)
+        {
+            new Event(Event.getLeafLevelClassName(this), "Clerk",
+                    "Could not instantiate Registry", Event.ERROR);
+        }
+
+
+        // STEP 3.2: Be sure to set the dependencies correctly
+        setDependencies();
+
+        // Set up the initial view
+        createAndShowTransactionChoiceView();
+    }
+
+    private void setDependencies()
+    {
+        dependencies = new Properties();
+        dependencies.setProperty("RegisterScout", "TransactionError");
+        dependencies.setProperty("UpdateScout", "TransactionError");
+        dependencies.setProperty("RemoveScout", "TransactionError");
+        dependencies.setProperty("AddTree", "TransactionError");
+        dependencies.setProperty("UpdateTree", "TransactionError");
+        dependencies.setProperty("RemoveTree", "TransactionError");
+        dependencies.setProperty("AddTreeType", "TransactionError");
+        dependencies.setProperty("UpdateTreeType", "TransactionError");
+        //remaining 3 use cases
+
+
+        myRegistry.setDependencies(dependencies);
+    }
+    public Object getState(String key)
+    {
+        if (key.equals("TransactionError") == true)
+        {
+            return transactionErrorMessage;
+        }
+        else
+        {
+            return "";
+        }
+    }
+    public void stateChangeRequest(String key, Object value) //creates and displays views
+    {
+        // Write the sCR method component for the key you
+        // just set up dependencies for
+        if (key.equals("CancelTransaction") == true)
+        {
+            createAndShowTransactionChoiceView();
+        }
+        else if (key.equals("RegisterScout") == true)
+        {
+            System.out.println(key);
+            String transType = key;
+
+            createAndShowInsertScoutTransactionView();
+        }
+        else if ((key.equals("UpdateScout") == true)|| (key.equals("RemoveScout")))
+        {
+            System.out.println(key);
+            String transType = key;
+
+            createAndShowSearchScoutView();
+        }
+        else if ((key.equals("OTHER USE CASES") == true))
+        {
+            //Todo: createAndShow views for other use cases
+        }
+		myRegistry.updateSubscribers(key, this);
+    }
+	
+    public void updateState(String key, Object value) //calls sCR to display view
+    {
+        stateChangeRequest(key, value);
+    }
+   /* public void doTransaction(String transactionType) {
+        try {
+            Transaction trans = TransactionFactory.createTransaction(
+                    transactionType);
+
+            trans.subscribe("CancelTransaction", this);
+            trans.stateChangeRequest("DoYourJob", "");
+        } catch (Exception ex) {
+            transactionErrorMessage = "FATAL ERROR: TRANSACTION FAILURE: Unrecognized transaction!!";
+            new Event(Event.getLeafLevelClassName(this), "createTransaction",
+                    "Transaction Creation Failure: Unrecognized transaction " + ex.toString(),
+                    Event.ERROR);
+        }
+    }*/
+   private void createAndShowTransactionChoiceView()
+   {
+       Scene currentScene = (Scene)myViews.get("TransactionChoiceView");
+
+       if (currentScene == null)
+       {
+           // create our initial view
+           View newView = ViewFactory.createView("TransactionChoiceView", this); // USE VIEW FACTORY
+           currentScene = new Scene(newView);
+           myViews.put("TransactionChoiceView", currentScene);
+       }
+
+       swapToView(currentScene);
+
+   }
+
+    private void createAndShowInsertScoutTransactionView()
+    {
+        Scene currentScene = (Scene)myViews.get("InsertScoutTransactionView");
+
+        if (currentScene == null)
+        {
+            // create our initial view
+            View newView = ViewFactory.createView("InsertScoutTransactionView", this); // USE VIEW FACTORY
+            currentScene = new Scene(newView);
+            myViews.put("InsertScoutTransactionView", currentScene);
+        }
+
+        swapToView(currentScene);
+
+    }
+
+    private void createAndShowSearchScoutView()
+    {
+        Scene currentScene = (Scene)myViews.get("SearchScoutView");
+
+        if (currentScene == null)
+        {
+            // create our initial view
+            View newView = ViewFactory.createView("SearchScoutView", this); // USE VIEW FACTORY
+            currentScene = new Scene(newView);
+            myViews.put("SearchScoutView", currentScene);
+        }
+
+        swapToView(currentScene);
+
+    }
+
+    public void subscribe(String key, IView subscriber)
+    {
+        myRegistry.subscribe(key, subscriber);
+    }
+
+    public void unSubscribe(String key, IView subscriber)
+    {
+        myRegistry.unSubscribe(key, subscriber);
+    }
+
+    public void swapToView(Scene newScene)
+    {
+
+        if (newScene == null)
+        {
+            System.out.println("Clerk.swapToView(): Missing view for display");
+            new Event(Event.getLeafLevelClassName(this), "swapToView",
+                    "Missing view for display ", Event.ERROR);
+            return;
+        }
+
+        myStage.setScene(newScene);
+        myStage.sizeToScene();
+
+
+        //Place in center
+        WindowPosition.placeCenter(myStage);
+
+    }
+
+}
---
+++ CSC_429_Term_Project/Term_Project/userinterface/TransactionChoiceView.java
@@ -0,0 +1,281 @@
+// specify the package
+package userinterface;
+
+// system imports
+import javafx.event.Event;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
+import javafx.stage.Stage;
+
+// project imports
+import impresario.IModel;
+
+/** The class containing the Transaction Choice View  for the Tree Lot Application */
+//==============================================================
+public class TransactionChoiceView extends View
+{
+	//GUI components
+	private Button registerScout;
+	private Button updateScout;
+	private Button removeScout;
+	private Button addTree;
+	private Button updateTree;
+	private Button removeTree;
+	private Button addTreeType;
+	private Button updateTreeType;
+	private Button startShift;
+	private Button endShift;
+	private Button sellTree;
+
+	// constructor for this class -- takes a model object
+	//----------------------------------------------------------
+	public TransactionChoiceView(IModel clerk)
+	{
+		super(clerk, "TransactionChoiceView");
+
+		// create a container for showing the contents
+		VBox container = new VBox(10);
+		container.setPadding(new Insets(15, 5, 5, 5));
+
+		// Add a title for this panel
+		container.getChildren().add(createTitle());
+		
+		// how do you add white space?
+		container.getChildren().add(new Label(" "));
+
+		// create our GUI components, add them to this Container
+		container.getChildren().add(createFormContents());
+
+		container.getChildren().add(createStatusLog("             "));
+
+		getChildren().add(container);
+
+		populateFields();
+
+		myModel.subscribe("TransactionError", this);
+	}
+
+	// Create the labels and fields
+	//-------------------------------------------------------------
+	private VBox createTitle()
+	{
+		VBox container = new VBox(10);
+		Text titleText = new Text("       Brockport Tree Lot         ");
+		titleText.setFont(Font.font("Arial", FontWeight.BOLD, 20));
+		titleText.setWrappingWidth(300);
+		titleText.setTextAlignment(TextAlignment.CENTER);
+		titleText.setFill(Color.BLACK);
+		container.getChildren().add(titleText);
+
+		Text welcomeText = new Text("Welcome!");
+		welcomeText.setFont(Font.font("Arial", FontWeight.BOLD, 20));
+		welcomeText.setWrappingWidth(300);
+		welcomeText.setTextAlignment(TextAlignment.CENTER);
+		welcomeText.setFill(Color.BLACK);
+		container.getChildren().add(welcomeText);
+
+		Text inquiryText = new Text("What do you wish to do today?");
+		inquiryText.setFont(Font.font("Arial", FontWeight.BOLD, 14));
+		inquiryText.setWrappingWidth(300);
+		inquiryText.setTextAlignment(TextAlignment.CENTER);
+		inquiryText.setFill(Color.BLACK);
+		container.getChildren().add(inquiryText);
+	
+		return container;
+	}
+
+
+	// Create the navigation buttons
+	//-------------------------------------------------------------
+	private VBox createFormContents()
+	{
+
+		VBox container = new VBox(15);
+
+		// create the buttons, listen for events, add them to the container
+		HBox dCont = new HBox(10);
+		dCont.setAlignment(Pos.CENTER);
+		registerScout = new Button("Register A Scout");
+		registerScout.setFont(Font.font("Arial", FontWeight.BOLD, 14));
+		registerScout.setOnAction(new EventHandler<ActionEvent>() {
+
+       		     @Override
+       		     public void handle(ActionEvent e) {
+       		     	myModel.stateChangeRequest("RegisterScout", null);
+            	     }
+        	});
+		dCont.getChildren().add(registerScout);
+
+		container.getChildren().add(dCont);
+
+		HBox wCont = new HBox(10);
+		wCont.setAlignment(Pos.CENTER);
+		updateScout = new Button("Update A Scout");
+		updateScout.setFont(Font.font("Arial", FontWeight.BOLD, 14));
+		updateScout.setOnAction(new EventHandler<ActionEvent>() {
+
+       		     @Override
+       		     public void handle(ActionEvent e) {
+       		     	myModel.stateChangeRequest("UpdateScout", null);
+            	     }
+        	});
+		wCont.getChildren().add(updateScout);
+
+		container.getChildren().add(wCont);
+
+		HBox tCont = new HBox(10);
+		tCont.setAlignment(Pos.CENTER);
+		removeScout = new Button("Remove A Scout");
+		removeScout.setFont(Font.font("Arial", FontWeight.BOLD, 14));
+		removeScout.setOnAction(new EventHandler<ActionEvent>() {
+
+       		     @Override
+       		     public void handle(ActionEvent e) {
+       		     	myModel.stateChangeRequest("RemoveScout", null);
+            	     }
+        	});
+		tCont.getChildren().add(removeScout);
+
+		container.getChildren().add(tCont);
+
+		HBox biCont = new HBox(10);
+		biCont.setAlignment(Pos.CENTER);
+		addTree = new Button("Add A Tree");
+		addTree.setFont(Font.font("Arial", FontWeight.BOLD, 14));
+		addTree.setOnAction(new EventHandler<ActionEvent>() {
+
+       		     @Override
+       		     public void handle(ActionEvent e) {
+       		     	myModel.stateChangeRequest("AddTree", null);
+            	     }
+        	});
+		biCont.getChildren().add(addTree);
+		container.getChildren().add(biCont);
+
+		HBox iscCont = new HBox(10);
+		iscCont.setAlignment(Pos.CENTER);
+		updateTree = new Button("Update A Tree");
+		updateTree.setFont(Font.font("Arial", FontWeight.BOLD, 14));
+		updateTree.setOnAction(new EventHandler<ActionEvent>() {
+
+       		     @Override
+       		     public void handle(ActionEvent e) {
+       		     	 myModel.stateChangeRequest("UpdateTree", null);
+            	     }
+        	});
+
+		iscCont.getChildren().add(updateTree);
+		container.getChildren().add(iscCont);
+
+		HBox rtCont = new HBox(10);
+		rtCont.setAlignment(Pos.CENTER);
+		removeTree = new Button("Remove A Tree");
+		removeTree.setFont(Font.font("Arial", FontWeight.BOLD, 14));
+		removeTree.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent e) {
+				myModel.stateChangeRequest("RemoveTree", null);
+			}
+		});
+
+		rtCont.getChildren().add(removeTree);
+		container.getChildren().add(rtCont);
+
+		HBox attCont = new HBox(10);
+		attCont.setAlignment(Pos.CENTER);
+		addTreeType = new Button("Add A Tree Type");
+		addTreeType.setFont(Font.font("Arial", FontWeight.BOLD, 14));
+		addTreeType.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent e) {
+				myModel.stateChangeRequest("AddTreeType", null);
+			}
+		});
+
+		attCont.getChildren().add(addTreeType);
+		container.getChildren().add(attCont);
+
+		HBox uttCont = new HBox(10);
+		uttCont.setAlignment(Pos.CENTER);
+		updateTreeType = new Button("Update A Tree Type");
+		updateTreeType.setFont(Font.font("Arial", FontWeight.BOLD, 14));
+		updateTreeType.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent e) {
+				myModel.stateChangeRequest("UpdateTreeType", null);
+			}
+		});
+
+		uttCont.getChildren().add(updateTreeType);
+		container.getChildren().add(uttCont);
+
+		//Todo: add buttons startShift, endShift and sellTree
+
+		return container;
+	}
+
+	// Create the status log field
+	//-------------------------------------------------------------
+	private MessageView createStatusLog(String initialMessage)
+	{
+
+		statusLog = new MessageView(initialMessage);
+
+		return statusLog;
+	}
+
+	//-------------------------------------------------------------
+	public void populateFields()
+	{
+
+	}
+	
+
+	//---------------------------------------------------------
+	public void updateState(String key, Object value)
+	{
+		if (key.equals("TransactionError") == true)
+		{
+			// display the passed text
+			displayErrorMessage((String)value);
+		}
+	}
+
+	/**
+	 * Display error message
+	 */
+	//----------------------------------------------------------
+	public void displayErrorMessage(String message)
+	{
+		statusLog.displayErrorMessage(message);
+	}
+
+	/**
+	 * Clear error message
+	 */
+	//----------------------------------------------------------
+	public void clearErrorMessage()
+	{
+		statusLog.clearErrorMessage();
+	}
+}
+
