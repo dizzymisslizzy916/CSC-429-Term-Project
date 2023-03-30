2cbae78360c7155bd63f729ef6f9c56f38449b2d
dizzymisslizzy916 <113558563+dizzymisslizzy916@users.noreply.github.com>
2023-03-30 13:01:48 -0400

    Add files via upload

---
+++ CSC_429_Term_Project/Term_Project/compile.bat
@@ -0,0 +1,11 @@
+
+"C:\Program Files\Java\jdk1.8.0_351\bin\javac.exe" -d classes -classpath classes;. common\*.java
+"C:\Program Files\Java\jdk1.8.0_351\bin\javac.exe" -d classes -classpath classes;. database\*.java
+"C:\Program Files\Java\jdk1.8.0_351\bin\javac.exe" -d classes -classpath classes;. event\*.java
+"C:\Program Files\Java\jdk1.8.0_351\bin\javac.exe" -d classes -classpath classes;. exception\*.java
+"C:\Program Files\Java\jdk1.8.0_351\bin\javac.exe" -d classes -classpath classes;. impresario\*.java
+"C:\Program Files\Java\jdk1.8.0_351\bin\javac.exe" -d classes -classpath classes;. model\*.java
+"C:\Program Files\Java\jdk1.8.0_351\bin\javac.exe" -d classes -classpath classes;. userinterface\*.java
+"C:\Program Files\Java\jdk1.8.0_351\bin\javac.exe" -d classes -classpath classes;. Utilities\*.java
+"C:\Program Files\Java\jdk1.8.0_351\bin\javac.exe" -classpath classes;. Main.java
+java -cp mariadb-java-client-3.0.3.jar;classes;. Main
