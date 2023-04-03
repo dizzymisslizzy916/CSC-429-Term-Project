d3f9ea1fc57291df58f4e01c91614c85d409ca94
Anh Hoang <82491499+ashb9505@users.noreply.github.com>
2023-04-03 11:01:29 -0400

    Add files via upload

---
+++ src/main/java/model/Librarian.java
@@ -0,0 +1,331 @@
+package model;
+
+import impresario.*;
+import javafx.scene.Scene;
+import java.util.Hashtable;
+import java.util.Properties;
+import javafx.stage.Stage;
+
+
+import exception.InvalidPrimaryKeyException;
+import exception.PasswordMismatchException;
+import event.Event;
+import userinterface.MainStageContainer;
+import userinterface.View;
+import userinterface.ViewFactory;
+import userinterface.WindowPosition;
+
+/** The class containing the Librarian */
+//==============================================================
+public class Librarian implements IView, IModel
+// This class implements all these interfaces (and does NOT extend 'EntityBase')
+// because it does NOT play the role of accessing the back-end database tables.
+// It only plays a front-end role. 'EntityBase' objects play both roles.
+{
+    // For Impresario
+    private Properties dependencies;
+    private ModelRegistry myRegistry;
+
+    // GUI Components
+    private Hashtable<String, Scene> myViews;
+    private Stage	  	myStage;
+
+    private String loginErrorMessage = "";
+    private String transactionErrorMessage = "";
+
+    // constructor for this class
+    //----------------------------------------------------------
+    public Librarian()
+    {
+        myStage = MainStageContainer.getInstance();
+        myViews = new Hashtable<String, Scene>();
+
+        // STEP 3.1: Create the Registry object - if you inherit from
+        // EntityBase, this is done for you. Otherwise, you do it yourself
+        myRegistry = new ModelRegistry("Teller");
+        if(myRegistry == null)
+        {
+            new Event(Event.getLeafLevelClassName(this), "Teller",
+                    "Could not instantiate Registry", Event.ERROR);
+        }
+
+
+        // STEP 3.2: Be sure to set the dependencies correctly
+        setDependencies();
+
+        // Set up the initial view
+        createAndShowLibrarianView();
+    }
+
+    //-----------------------------------------------------------------------------------
+    private void setDependencies()
+    {
+//        dependencies = new Properties();
+//        dependencies.setProperty("InsertNewBook", "InsertError");
+//        dependencies.setProperty("InsertNewPatron", "InsertError");
+//        dependencies.setProperty("SearchForBook", "SearchError");
+//        dependencies.setProperty("SearchForPatron", "SearchError");
+//
+//        myRegistry.setDependencies(dependencies);
+    }
+
+
+    /**
+     * Method called from client to get the value of a particular field
+     * held by the objects encapsulated by this object.
+     *
+     * @param	key	Name of database column (field) for which the client wants the value
+     *
+     * @return	Value associated with the field
+     */
+    //----------------------------------------------------------
+    public Object getState(String key)
+    {
+
+        return "";
+    }
+
+    //----------------------------------------------------------------
+    public void stateChangeRequest(String key, Object value)
+    {
+        // STEP 4: Write the sCR method component for the key you
+        // just set up dependencies for
+        // DEBUG System.out.println("Teller.sCR: key = " + key);
+
+
+        if (key.equals("CancelTransaction") == true)
+        {
+            createAndShowLibrarianView();
+        }
+
+        else if (key.equals("InsertScout") == true)
+        {
+            System.out.println(key);
+            String transType = key;
+//            doTransaction(transType);
+            createAndShowScoutView();
+
+        }
+
+        else if (key.equals("SearchScout") == true)
+        {
+            System.out.println(key);
+            String transType = key;
+            doTransaction(transType);
+
+            //createAndShowPatronSearchView();
+        }
+
+        else if (key.equals("InsertPatron") == true)
+        {
+            System.out.println(key);
+            String transType = key;
+//            doTransaction(transType);
+
+            createAndShowPatronView();
+        }
+        else if (key.equals("InsertBook") == true)
+        {
+            System.out.println(key);
+            String transType = key;
+//            doTransaction(transType);
+
+            createAndShowBookView();
+        }
+
+        else if (key.equals("SearchBook") == true)
+        {
+            System.out.println(key);
+            String transType = key;
+            doTransaction(transType);
+
+            //createAndShowBookSearchView();
+        }
+
+        else if (key.equals("SearchPatron") == true)
+        {
+            System.out.println(key);
+            String transType = key;
+            doTransaction(transType);
+
+            //createAndShowPatronSearchView();
+        }
+
+
+        myRegistry.updateSubscribers(key, this);
+    }
+
+    /** Called via the IView relationship */
+    //----------------------------------------------------------
+    public void updateState(String key, Object value)
+    {
+        // DEBUG System.out.println("Teller.updateState: key: " + key);
+
+        stateChangeRequest(key, value);
+    }
+
+
+    /**
+     * Create a Transaction depending on the Transaction type (deposit,
+     * withdraw, transfer, etc.). Use the AccountHolder holder data to do the
+     * create.
+     */
+    //----------------------------------------------------------
+    public void doTransaction(String transactionType) {
+        try {
+            Transaction trans = TransactionFactory.createTransaction(
+                    transactionType);
+
+            trans.subscribe("CancelTransaction", this);
+            trans.stateChangeRequest("DoYourJob", "");
+        } catch (Exception ex) {
+            transactionErrorMessage = "FATAL ERROR: TRANSACTION FAILURE: Unrecognized transaction!!";
+            new Event(Event.getLeafLevelClassName(this), "createTransaction",
+                    "Transaction Creation Failure: Unrecognized transaction " + ex.toString(),
+                    Event.ERROR);
+        }
+    }
+
+    //------------------------------------------------------------
+    private void createAndShowLibrarianView()
+    {
+        Scene currentScene = (Scene)myViews.get("LibrarianView");
+
+        if (currentScene == null)
+        {
+            // create our initial view
+            View newView = ViewFactory.createView("LibrarianView", this); // USE VIEW FACTORY
+            currentScene = new Scene(newView);
+            myViews.put("LibrarianView", currentScene);
+        }
+
+        swapToView(currentScene);
+
+    }
+
+    private void createAndShowScoutView()
+    {
+        Scene currentScene = (Scene)myViews.get("InsertScoutView");
+
+        if (currentScene == null)
+        {
+            // create our initial view
+            View newView = ViewFactory.createView("InsertScoutView", this); // USE VIEW FACTORY
+            currentScene = new Scene(newView);
+            myViews.put("InsertScoutView", currentScene);
+        }
+
+        swapToView(currentScene);
+
+    }
+
+    private void createAndShowPatronView()
+    {
+        Scene currentScene = (Scene)myViews.get("InsertPatronView");
+
+        if (currentScene == null)
+        {
+            // create our initial view
+            View newView = ViewFactory.createView("InsertPatronView", this); // USE VIEW FACTORY
+            currentScene = new Scene(newView);
+            myViews.put("InsertPatronView", currentScene);
+        }
+
+        swapToView(currentScene);
+
+    }
+
+    private void createAndShowBookView()
+    {
+        Scene currentScene = (Scene)myViews.get("InsertBookView");
+
+        if (currentScene == null)
+        {
+            // create our initial view
+            View newView = ViewFactory.createView("InsertBookView", this); // USE VIEW FACTORY
+            currentScene = new Scene(newView);
+            myViews.put("InsertBookView", currentScene);
+        }
+
+        swapToView(currentScene);
+
+    }
+
+    private void createAndShowBookSearchView()
+    {
+        Scene currentScene = (Scene)myViews.get("SearchBookView");
+
+        if (currentScene == null)
+        {
+            // create our initial view
+            View newView = ViewFactory.createView("SearchBookView", this); // USE VIEW FACTORY
+            currentScene = new Scene(newView);
+            myViews.put("SearchBookView", currentScene);
+        }
+
+        swapToView(currentScene);
+
+    }
+
+    private void createAndShowPatronSearchView()
+    {
+        Scene currentScene = (Scene)myViews.get("SearchPatronView");
+
+        if (currentScene == null)
+        {
+            // create our initial view
+            View newView = ViewFactory.createView("SearchPatronView", this); // USE VIEW FACTORY
+            currentScene = new Scene(newView);
+            myViews.put("SearchPatronView", currentScene);
+        }
+
+        swapToView(currentScene);
+
+    }
+
+
+
+
+    /** Register objects to receive state updates. */
+    //----------------------------------------------------------
+    public void subscribe(String key, IView subscriber)
+    {
+        // DEBUG: System.out.println("Cager[" + myTableName + "].subscribe");
+        // forward to our registry
+        myRegistry.subscribe(key, subscriber);
+    }
+
+    /** Unregister previously registered objects. */
+    //----------------------------------------------------------
+    public void unSubscribe(String key, IView subscriber)
+    {
+        // DEBUG: System.out.println("Cager.unSubscribe");
+        // forward to our registry
+        myRegistry.unSubscribe(key, subscriber);
+    }
+
+
+
+    //-----------------------------------------------------------------------------
+    public void swapToView(Scene newScene)
+    {
+
+
+        if (newScene == null)
+        {
+            System.out.println("Teller.swapToView(): Missing view for display");
+            new Event(Event.getLeafLevelClassName(this), "swapToView",
+                    "Missing view for display ", Event.ERROR);
+            return;
+        }
+
+        myStage.setScene(newScene);
+        myStage.sizeToScene();
+
+
+        //Place in center
+        WindowPosition.placeCenter(myStage);
+
+    }
+
+}
