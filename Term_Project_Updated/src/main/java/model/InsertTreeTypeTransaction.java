85005e5cebdaa7e0e2c18ef1ba0ef4df25129b7f
Anh Hoang <82491499+ashb9505@users.noreply.github.com>
2023-04-04 13:26:04 -0400

    Add files via upload

---
+++ Term_Project_Updated/src/main/java/model/InsertTreeTypeTransaction.java
@@ -0,0 +1,130 @@
+// specify the package
+package model;
+
+// system imports
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import java.util.Properties;
+import java.util.Vector;
+
+// project imports
+import event.Event;
+import exception.InvalidPrimaryKeyException;
+
+import userinterface.View;
+import userinterface.ViewFactory;
+
+/** The class containing the WithdrawTransaction for the ATM application */
+//==============================================================
+public class InsertTreeTypeTransaction extends Transaction
+{
+    private TreeType myTreeType;
+
+    // GUI Components
+
+    private String transactionErrorMessage = "";
+
+    /**
+     * Constructor for this class.
+     *
+     *
+     */
+    //----------------------------------------------------------
+    public InsertTreeTypeTransaction()
+            throws Exception
+    {
+        super();
+    }
+
+    //----------------------------------------------------------
+    protected void setDependencies()
+    {
+        dependencies = new Properties();
+        dependencies.setProperty("TreeTypeData", "TransactionError");
+        dependencies.setProperty("OK", "CancelTransaction");
+
+        myRegistry.setDependencies(dependencies);
+    }
+
+    /**
+     * This method encapsulates all the logic of creating the account,
+     * verifying ownership, crediting, etc. etc.
+     */
+    //----------------------------------------------------------
+    public void processTransaction(Properties props) {
+        String sentBPfix = props.getProperty("barcodePrefix");
+        try
+        {
+            System.out.println("Getting here 2");
+            TreeType oldtreeType = new TreeType(sentBPfix);
+            transactionErrorMessage = "ERROR: Tree type with barcode prefix: " + sentBPfix + " already exists";
+        }
+        catch (Exception ex)
+        {
+            System.out.println("Getting here 3");
+            myTreeType = new TreeType(props);
+            myTreeType.update();
+            transactionErrorMessage = (String)myTreeType.getState("UpdateStatusMessage");
+
+        }
+    }
+
+    //-----------------------------------------------------------
+    public Object getState(String key)
+    {
+        if (key.equals("TransactionError") == true)
+        {
+            return transactionErrorMessage;
+        }
+
+
+        return null;
+    }
+
+    //-----------------------------------------------------------
+    public void stateChangeRequest(String key, Object value)
+    {
+        System.out.println("Getting here 0");
+        if (key.equals("DoYourJob") == true)
+        {
+            doYourJob();
+        }
+        else
+        if (key.equals("TreeTypeData") == true)
+        {
+            System.out.println("Getting here 1");
+            processTransaction((Properties)value);
+        }
+
+        myRegistry.updateSubscribers(key, this);
+    }
+
+    /**
+     * Create the view of this class. And then the super-class calls
+     * swapToView() to display the view in the stage
+     */
+    //------------------------------------------------------
+    protected Scene createView()
+    {
+        Scene currentScene = myViews.get("InsertTreeTypeView");
+
+        if (currentScene == null)
+        {
+            // create our new view
+            View newView = ViewFactory.createView("InsertTreeTypeView", this);
+            currentScene = new Scene(newView);
+            myViews.put("InsertTreeTypeView", currentScene);
+
+            return currentScene;
+        }
+        else
+        {
+            return currentScene;
+        }
+    }
+
+    //------------------------------------------------------
+
+}
+
+
