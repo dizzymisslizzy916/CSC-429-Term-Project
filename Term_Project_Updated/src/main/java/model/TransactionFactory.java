505343d9c660160cb036064c30f2f9283f30b2c9
dizzymisslizzy916 <113558563+dizzymisslizzy916@users.noreply.github.com>
2023-04-04 10:40:51 -0400

    Add files via upload

---
+++ Term_Project_Updated/src/main/java/model/TransactionFactory.java
@@ -0,0 +1,54 @@
+// specify the package
+package model;
+
+// system imports
+import userinterface.InsertPatronView;
+
+
+import java.util.Vector;
+import javax.swing.JFrame;
+
+// project imports
+
+/** The class containing the TransactionFactory for the ATM application */
+//==============================================================
+public class TransactionFactory
+{
+
+	/**
+	 *
+	 */
+	//----------------------------------------------------------
+	public static Transaction createTransaction(String transType) throws Exception
+	{
+		Transaction retValue = null;
+
+		if (transType.equals("InsertPatron") == true)
+		{
+			retValue = new InsertPatronTransaction();
+		}
+		else if (transType.equals("InsertScout") == true)
+		{
+			retValue = new InsertBookTransaction();
+		}
+		else if (transType.equals("SearchScout") == true)
+		{
+			retValue = new SearchScoutTransaction();
+		}
+		else if (transType.equals("InsertBook") == true)
+		{
+			retValue = new InsertBookTransaction();
+		}
+		else if (transType.equals("SearchBook") == true)
+		{
+			retValue = new SearchBookTransaction();
+		}
+		else if (transType.equals("SearchPatron") == true)
+		{
+			retValue = new SearchPatronTransaction();
+		}
+
+
+		return retValue;
+	}
+}
